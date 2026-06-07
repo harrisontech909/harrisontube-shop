e8337e142b33b51491b81488e52883d596f2cf10
Harrison Lunn <harrisontech909@gmail.com>
2026-06-07 21:27:49 +0100

    I Am H

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,32 @@
+'use client';
+
 import "./globals.css";
+import { useState } from "react";
 
 export default function Home() {
+    const [counter, setCounter] = useState(0);
+
+    const handleClick = () => {
+        setCounter(counter + 1);
+    };
+
     return (
         <main>
+            <h3 className="first-subtitle">
+                    Hi I'm Harrison!
+                </h3>
             <h1 className="main-title">
                 Welcome to HarrisonTube Shop
             </h1>
+            <h1>{counter}</h1>
+            <button onClick={handleClick} className="main-button">
+                Click on Me
+            </button>
             <div className="first-container">
                 <h2 className="first-title">
                     First why don't you check out our HarrisonTube channel?
                 </h2>
+                
                 <p className="first-description">
                     There you can find all the latest videos about me.
                 </p>
